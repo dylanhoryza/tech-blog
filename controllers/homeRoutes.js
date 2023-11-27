dbf965a993c768d71e2d0d8d17ccd74680b061c5
dylan horyza <horyza.dylan@gmail.com>
2023-11-27 14:56:49 -0700

    updated get route for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
+const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  res.render('homepage');
+  try {
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    res.render('homepage', {
+      blogs,
+      // logged_in: req.session.logged_in
+    });
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 module.exports = router;
