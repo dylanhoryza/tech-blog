588869a96e40d9975cc59a7b25756a41cab94f31
dylan horyza <horyza.dylan@gmail.com>
2023-12-12 11:23:45 -0700

    created update function

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   const newPostButton = document.querySelector('.new-btn');
   const contentSection = document.getElementById('contentSection');
+  const updateSection = document.getElementById('updateSection');
+  const updateButtons = document.querySelectorAll('.update-blog');
+  const submitButtons = document.querySelectorAll('.post-update-btn');
 
   newPostButton.addEventListener('click', function() {
     console.log('clicked')
@@ -12,7 +15,25 @@ document.addEventListener('DOMContentLoaded', function() {
   submitBlog.addEventListener('click', async function(event) {
     event.preventDefault();
     await postBlog();
+  });
+
+
+  updateButtons.forEach(updateButton => {
+  updateButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const updateSection = updateButton.parentElement.nextElementSibling;
+    updateSection.classList.toggle('hide');
+  });
+});
+
+submitButtons.forEach(submitButton => {
+  submitButton.addEventListener('click', async function(event) {
+    event.preventDefault();
+    const blogId = submitButton.dataset.blogId;
+    await updateBlog(blogId);
   })
+})
+
 });
 
 // Create new Blog post
@@ -39,4 +60,33 @@ async function postBlog() {
     alert('Error occured while creating new blog')
     
   }
+}
+
+
+// Update blog function
+async function updateBlog(blogId) {
+  const title = document.querySelector(`#blog-title-update-${blogId}`).value;
+  const description = document.querySelector(`#blog-content-update-${blogId}`).value;
+  // const blogId = event.target.dataset.blogId;
+
+  if (blogId !== null) {
+  try {
+    const response = await fetch(`/api/blog/${blogId}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      console.log('blog updated');
+    }
+    
+  } catch (error) {
+    console.log('Error:', error);
+    alert('Error occured while updating blog')
+  }
+  }
+   
 }
